541b3616d19cfd8cedb85bb8e28e76197b1be455
asobrabj <j.barbosasilva@yahoo.com.br>
2023-07-16 20:23:47 -0300

    Primeiro commit - Base para construir o projeto Hello World

---
+++ README.md
@@ -0,0 +1,11 @@
+# Componentes e Props no React 
+
+- Configuração do ambiente
+- Como Funciona o React
+- Componentes e funções
+- Composição de componentes
+
+## 10 formas de dizer "Hello World"
+
+
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSanbox</h1>
+      <h2>Start editing to see some magic happen</h2>
+    </div>
+  );
+}
+
+export default App;
